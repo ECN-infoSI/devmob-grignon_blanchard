69cc2ad4a14f7bdc54bbd65de3b88b1b46ab9de8
grigm <manon.grignon@eleves.ec-nantes.fr>
2025-03-21 09:44:44 +0100

    navigation page rayon vers page produit

---
+++ app/src/main/java/com/example/seniormarket/ui/theme/OrderViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.seniormarket.ui.theme
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class OrderViewModel : ViewModel() {
+    /**
+     * Cupcake state for this order
+     */
+   /* private val _uiState
+    val uiState: StateFlow<OrderUiState> = _uiState.asStateFlow()
+*/
+}
